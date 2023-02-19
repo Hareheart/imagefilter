e5f37720ce74f0918f33df2eb64dc44d2d4974bc
Blaze <68640945+thebeastof@users.noreply.github.com>
2023-02-19 14:58:46 +0300

    made some changes

--- transform.py
+++ transform.py
@@ -9,14 +9,22 @@ def main():
     # Show image
     image.show()
 
+    width, height = image.size
+    new_image = Image.new("RGB", (image.size), "white")
+
     if sys.argv[1] == "flip_image":
-        flip_image(image)
+        flip_image(image, width, height, new_image)
     elif sys.argv[1] == "shrink_image":
         shrink_image(image)
 
 
-def flip_image():
-    pass
+def flip_image(image, width, height, new_image):
+    t = []
+    a = 0
+    for x in range(width):
+        t.append(image.getpixel(x, 0))
+        
+
 
 def shrink_image():
     pass
